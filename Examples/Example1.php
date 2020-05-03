0ca259ed51a451926a73a327f1b9cb7730e4a1a0
wes <weshenshall@gmail.com>
2020-05-03 16:15:49 +0200

    added getters and setters

--- Examples/Example1.php
+++ Examples/Example1.php
@@ -3,4 +3,5 @@ namespace Henshall\Timezones;
 require("../src/Timezones.php");
 $timezones = new Timezones("localhost", "root", "root", "database");
 $zones = $timezones->get_local_timezone();
+var_dump($zones);
  ?>
