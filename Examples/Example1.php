04d9b12ccbf898667634a4679e063ef0d43d6064
wes <weshenshall@gmail.com>
2020-05-03 16:09:40 +0200

    updated readme, moved timezoneArray into seperate file, and turned public variable to private

---
+++ Examples/Example1.php
@@ -0,0 +1,6 @@
+<?php 
+namespace Henshall\Timezones;
+require("../src/Timezones.php");
+$timezones = new Timezones("localhost", "root", "root", "database");
+$zones = $timezones->get_local_timezone();
+ ?>
